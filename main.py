078fe25d7f00fb116d5bde11340976d14e963cb2
sinayt22 <sinaytwork@gmail.com>
2026-02-11 10:06:33 -0500

    Add player with rotation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 
 def main():
@@ -8,6 +9,8 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         log_state()
@@ -17,6 +20,10 @@ def main():
                 return
 
         screen.fill("black")
+
+        player.update(dt)
+        player.draw(screen)
+
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000

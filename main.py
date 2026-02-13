c4a9d8f53e8c0f66e370898490dd72e25eb67373
sinayt22 <sinaytwork@gmail.com>
2026-02-13 16:23:28 -0500

    add asteroid split

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ def main():
             for shot in shots:
                 if asteroid.collide_with(shot):
                     log_event("asteroid_shot")
-                    asteroid.kill()
+                    asteroid.split()
                     shot.kill()
 
         for d in drawable:

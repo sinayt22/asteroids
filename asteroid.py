fe9881b56491c9fecf9b63df80784556bc11aa9e
sinayt22 <sinaytwork@gmail.com>
2026-03-02 09:09:21 -0500

    Add texture and shape to asteroids

--- asteroid.py
+++ asteroid.py
@@ -10,9 +10,37 @@ from screen_wrapper import ScreenWrapper
 class Asteroid(CircleShape, ScreenWrapper):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
+        self.points = self.generate_asteroid_shape()
+
+    def generate_asteroid_shape(self):
+        points = []
+        num_points = random.randint(8, 12)
+
+        for i in range(num_points):
+            angle = (360 / num_points) * i
+            variation = random.uniform(0.7, 1.0)
+            distance = self.radius * variation
+            point = pygame.Vector2(distance, 0).rotate(angle)
+            points.append(point)
+
+        return points
 
     def draw(self, screen):
-        pygame.draw.circle(screen, "white", self.position, self.radius, LINE_WIDTH)
+        size = int(self.radius * 2)
+        temp_surface = pygame.Surface((size, size), pygame.SRCALPHA)
+
+        relative_points = [
+            (self.radius, self.radius) + offset for offset in self.points
+        ]
+        pygame.draw.polygon(temp_surface, (255, 255, 255), relative_points, 0)
+
+        texture = pygame.image.load("asteroid_texture.png").convert_alpha()
+        texture = pygame.transform.scale(texture, (size, size))
+
+        temp_surface.blit(texture, (0, 0), special_flags=pygame.BLEND_RGBA_MULT)
+        screen.blit(
+            temp_surface, (self.position.x - self.radius, self.position.y - self.radius)
+        )
 
     def update(self, dt):
         self.wrap_position()
